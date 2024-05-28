3a6dbb734a56598a69080fa8e9887485d6b7f5ba
eliasnsz <eliasnsz.dev@gmail.com>
2024-05-28 13:01:32 -0300

    :test_tube: feat(test): add e2e database configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	plugins: [tsconfigPaths()],
 	test: {
 		globals: true,
+		environmentMatchGlobs: [["src/**", "prisma"]],
+		include: ["**/*.e2e-{test,spec}.{js,ts}"],
 	},
-	plugins: [tsconfigPaths()],
 });
