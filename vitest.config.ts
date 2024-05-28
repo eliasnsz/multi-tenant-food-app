66d5faee7ce584af47f1d2c444a4be635e833a07
eliasnsz <eliasnsz.dev@gmail.com>
2024-05-28 10:52:02 -0300

    :test_tube: chore(test): install and setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	test: {
+		globals: true,
+	},
+	plugins: [tsconfigPaths()],
+});
